96035b0e4a95a47512004b01c8acf5d23d35211e
Will Maier <189851+whilp@users.noreply.github.com>
2026-03-25 15:09:41 -0700

    Modernize terminal UI with thin box-drawing lines (#6)

--- qestyles.h
+++ qestyles.h
@@ -5,10 +5,10 @@
               QERGB(0xf8, 0xd8, 0xb0), QERGB(0x00, 0x00, 0x00), QE_FONT_FAMILY_FIXED, 12)
 
     /* system styles */
-    STYLE_DEF(QE_STYLE_MODE_LINE, "mode-line", /* black on grey88 */
-              QERGB(0x00, 0x00, 0x00), QERGB(0xe0, 0xe0, 0xe0), 0, 0)
-    STYLE_DEF(QE_STYLE_WINDOW_BORDER, "window-border", /* black on grey88 */
-              QERGB(0x00, 0x00, 0x00), QERGB(0xe0, 0xe0, 0xe0), 0, 0)
+    STYLE_DEF(QE_STYLE_MODE_LINE, "mode-line", /* grey74 on dark grey */
+              QERGB(0xb0, 0xb0, 0xb0), QERGB(0x1a, 0x1a, 0x1a), 0, 0)
+    STYLE_DEF(QE_STYLE_WINDOW_BORDER, "window-border", /* dim cyan on dark grey */
+              QERGB(0x60, 0x90, 0x90), QERGB(0x1a, 0x1a, 0x1a), 0, 0)
     STYLE_DEF(QE_STYLE_MINIBUF, "minibuf", /* yellow */
               QERGB(0xff, 0xff, 0x00), COLOR_TRANSPARENT, 0, 0)
     STYLE_DEF(QE_STYLE_STATUS, "status", /* yellow */
@@ -23,8 +23,8 @@
     /* coloring styles */
     STYLE_DEF(QE_STYLE_HIGHLIGHT, "highlight", /* black on #f8d8b0 */
               QERGB(0x00, 0x00, 0x00), QERGB(0xf8, 0xd8, 0xb0), 0, 0)
-    STYLE_DEF(QE_STYLE_SELECTION, "selection", /* white on blue */
-              QERGB(0xff, 0xff, 0xff), QERGB(0x00, 0x00, 0xff), 0, 0)
+    STYLE_DEF(QE_STYLE_SELECTION, "selection", /* white on dark blue */
+              QERGB(0xff, 0xff, 0xff), QERGB(0x26, 0x4f, 0x78), 0, 0)
 
     /* Generic syntax coloring styles */
     STYLE_DEF(QE_STYLE_COMMENT, "comment", /* #f84400 */
